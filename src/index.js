9f328c0dcab86138cd5d02b6c698d7bdf4d1e4f2
cindytrac <cindy.trac.11@gmail.com>
2022-10-12 23:16:34 -0400

    thing

--- src/index.js
+++ src/index.js
@@ -9,24 +9,3 @@ root.render(
         <App />
     </BrowserRouter>
 );
-<<<<<<< HEAD
-
-const view = function( e ) {
-    e.preventDefault()
-    fetch('/view', {
-        method:'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-}
-
-window.onload = function() {
-    console.log("HERE")
-    let viewButton = document.querySelectorAll(".view-button")
-    viewButton.forEach(element => {
-        element.onclick = view
-    });
-}
-=======
->>>>>>> 74c5f47e0f9786c89b45061db8dfa96a3f64d55e
